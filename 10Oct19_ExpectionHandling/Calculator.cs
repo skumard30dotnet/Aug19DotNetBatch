8c170473aba856ab2cbb830fb0a5e6a5a5d37704
skumard30@yahoo.co.in <skumard30@yahoo.co.in>
2019-10-10 08:06:53 +0530

    Commit

---
+++ 10Oct19_ExpectionHandling/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10Oct19_ExpectionHandling
+{
+    class Calculator
+    {
+        //public void Division(int a, int b)
+        //{
+        //    if (b > 0)
+        //    {
+        //        int c = a / b;
+
+        //        Console.WriteLine("The division value is {0}", c);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("The Denominator cannot be zero");
+        //    }
+
+        //}
+
+        public void Division(int a, int b)
+        {
+            try
+            {
+                int c = a / b;
+                Console.WriteLine("The division value is {0}", c);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The denominator cannot be zero ({0})", e.Message);
+                Console.WriteLine("The stack trace is {0}", e.StackTrace);
+            }
+        }
+
+
+        public void Division1(int a, int b)
+        {
+            try
+            {
+                int c = a / b;
+                Console.WriteLine("The division value is {0}", c);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The denominator cannot be zero");
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
---
+++ 10Oct19_ExpectionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// ArgumentException	Raised when a non-null argument that is passed to a method is invalid.
+//ArgumentNullException Raised when null argument is passed to a method.
+//ArgumentOutOfRangeException Raised when the value of an argument is outside the range of valid values.
+//DivideByZeroException Raised when an integer value is divide by zero.
+//FileNotFoundException Raised when a physical file does not exist at the specified location.
+//FormatException Raised when a value is not in an appropriate format to be converted from a string by a conversion method such as Parse.
+//IndexOutOfRangeException Raised when an array index is outside the lower or upper bounds of an array or collection.
+//InvalidOperationException Raised when a method call is invalid in an object's current state.
+//InvalidCastException Raised when incompitible types are being converted.
+//KeyNotFoundException Raised when the specified key for accessing a member in a collection is not exists.
+//NotSupportedException Raised when a method or operation is not supported.
+//NullReferenceException Raised when program access members of null object.
+//OverflowException Raised when an arithmetic, casting, or conversion operation results in an overflow.
+//OutOfMemoryException Raised when a program does not get enough memory to execute the code.
+//StackOverflowException Raised when a stack in memory overflows.
+//TimeoutException The time interval allotted to an operation has expired.
+/// </summary>
+
+namespace _10Oct19_ExpectionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Division(10,0);
+
+            Console.Read();
+        }
+    }
+}
